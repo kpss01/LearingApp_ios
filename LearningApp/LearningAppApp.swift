967e319d344eee68fdf4d0cbeab019c171d77c7f
kpss <saraohealthcare@gmail.com>
2021-06-07 12:49:56 +0530

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by karanpreet singh sarao on 07/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
