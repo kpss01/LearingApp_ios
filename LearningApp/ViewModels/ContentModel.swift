78b76d71c175f119537bd6d1761252c28a7d9786
kpss <saraohealthcare@gmail.com>
2021-06-07 12:58:54 +0530

    initial setup

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by karanpreet singh sarao on 07/06/21.
+//
+
+import Foundation
+
+class ContentModel : ObservableObject{
+    
+    
+    
+}
