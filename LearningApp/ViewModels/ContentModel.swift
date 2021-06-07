ab1b8bd3e36634156f1519377368c8dae88211cb
kpss <saraohealthcare@gmail.com>
2021-06-07 22:42:16 +0530

    add data file, assets and create model class

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,6 +9,48 @@ import Foundation
 
 class ContentModel : ObservableObject{
     
+    @Published var modules = [Module]()
     
+    var styleData:Data?
+    
+    init() {
+        
+        getLocalData()
+        
+    }
+    
+    func getLocalData(){
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do{
+            
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+            
+        }catch{
+            
+            print(error)
+            
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do{
+            
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+        }catch{
+            print("Error in style file")
+        }
+        
+    }
     
 }
